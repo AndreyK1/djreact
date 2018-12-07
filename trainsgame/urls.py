64ce4e3ef424b71873839e2af3fe90b22dfc0593
Andrey <ponachen@mail.ru>
2018-12-07 14:54:03 +0300

    playGround step53 !!! Front/Back (auth)

--- trainsgame/urls.py
+++ trainsgame/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from .views import (
     beginOfGAme,
-)
+    log_in, log_out, sign_up)
 
 urlpatterns = [
 	# url(r'^$', UserViewSet.as_view(), name='us_list'),
     # url(r'^trains/', beginOfGAme, name='beginOfGAme'),
     url(r'^$', beginOfGAme, name='beginOfGAme'),
+    url(r'^log_in/$', log_in, name='log_in'),
+    url(r'^log_out/$', log_out, name='log_out'),
+    url(r'^sign_up/$', sign_up, name='sign_up'),
 ]
--- trainsgame/views.py
+++ trainsgame/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
 
+from django.contrib.auth import login, logout
 # Create your views here.
+from django.urls import reverse
+# from django.core.urlresolvers import reverse
+
 from trainsgame.models import PlayGround
 
 
@@ -14,3 +19,34 @@ def beginOfGAme(request):
     # md2.increase();
     # template_name = 'post_detail.html'
     # return render(request, "SingleDot.html", {"dd": 3, "sdot" : model["dot"], "ffff" : md2.doter })
+
+
+
+def log_in(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect(reverse('trains:beginOfGAme'))
+        else:
+            print(form.errors)
+    return render(request, 'log_in.html', {'form': form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse('trains:log_in'))
+
+
+def sign_up(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('trains:log_in')
+        else:
+            print(form.errors)
+    return render(request, "sign_up.html", {"form": form})
+    # return render(request, "TrainGame.html", {"dd": 3})
