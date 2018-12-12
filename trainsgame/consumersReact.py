1929c41df46296bf9ec88fdfb93692a86253e05f
Andrey <ponachen@mail.ru>
2018-12-12 11:09:04 +0300

    react consumer1

--- trainsgame/consumersReact.py
+++ trainsgame/consumersReact.py
@@ -8,15 +8,22 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.layers import get_channel_layer
 
+# from channels import Group
+
+
 from trainsgame.models import PlayGroundList
 
+
+# https://django-channels-presence.readthedocs.io/en/latest/
 
 class GroupConsumerReact(AsyncJsonWebsocketConsumer):
 
     GROUP = "react"
     STARTET = False
+    CONNECTED = 0
 
     async def connect(self,text_data=None):
+        GroupConsumerReact.CONNECTED +=1
         await self.accept()
         await self.channel_layer.group_add(self.GROUP, self.channel_name)
         print("--------Added " +self.channel_name+" channel to react")
@@ -52,15 +59,25 @@ class GroupConsumerReact(AsyncJsonWebsocketConsumer):
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.GROUP, self.channel_name)
-        print("--------------------------Removed "+self.channel_name+" channel to trains")
+        GroupConsumerReact.CONNECTED -=1
+        print("--------------------------Removed "+self.channel_name+" channel to react")
 
     async def sendsArenas(self):
-        while True:
+        channel_layer = get_channel_layer()
+        # old_group = Group('old_friends')
+        # ch_group_list = channel_layer.group_channels(self.GROUP)
+
+        if GroupConsumerReact.STARTET == True:
+            return
+
+        GroupConsumerReact.STARTET = True
+
+        while GroupConsumerReact.CONNECTED>0:
 
             await asyncio.sleep(3)
             playgrounds = PlayGroundList().PlayGrounds
-            print("sendsArenas")
-            channel_layer = get_channel_layer()
+            print("sendsArenas self.CONNECTED " + str(GroupConsumerReact.CONNECTED))
+            # channel_layer = get_channel_layer()
 
             serialized_obj = json.dumps(playgrounds, default=lambda x: x.__dict__)
 
@@ -72,6 +89,8 @@ class GroupConsumerReact(AsyncJsonWebsocketConsumer):
                 str(self.GROUP), {"type": "user.react",
                                         "event": {"value": serialized_obj, "type": "arenas"},
                                         })
+
+        GroupConsumerReact.STARTET = False
 
     # обработка каждого коннекта из канала
     async def user_react(self, event):
