a4c3c56cf5e5639862c8d3e7f8d9424d2812f680
Andrey <ponachen@mail.ru>
2019-06-27 14:13:16 +0300

    show contract count by web3 new2

--- trainsgame/rejs/components/ProfileModal.jsx
+++ trainsgame/rejs/components/ProfileModal.jsx
@@ -9,11 +9,12 @@ export default class ProfileModal extends React.Component {
         let tokenABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimalKoef","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_value","type":"uint256"}],"name":"burnFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getDecimalKoef","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"}]
         let tokenAddress = "0xab20a3630f943831a57c767ee94b90801b88787c"
 
+        let wallet = '0x45ca230b2684D10bE2F8a749CA2F60828A2576f4' ;
 
 
-        let web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/metamask'));
-        window.TreasureContractNew1 = new web3.eth.Contract(tokenABI, tokenAddress);
-
+        window.web31 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/metamask'));
+        window.TreasureContractNew1 = new window.web31.eth.Contract(tokenABI, tokenAddress, {from: wallet});
+        // window.web3 = web3
         // window.TreasureContractOld = window.TreasureContractNew1;
 
 
@@ -90,9 +91,8 @@ export default class ProfileModal extends React.Component {
       //
       //
       // let TreasureContra = new web3.eth.Contract(tokenABI, tokenAddress);
-         console.log(" window.TreasureContractNew1 111123",  window.TreasureContractNew1)
+         console.log(" window.TreasureContractNew1 1111234",  window.TreasureContractNew1)
        window.TreasureContractNew1.methods.name().call().then(console.log);
-
       //old working
       // window.TreasureContractOld.name(function(error, result){
       //           console.log("name ", result)
@@ -110,10 +110,38 @@ export default class ProfileModal extends React.Component {
 
       let toAddress = "0xc62A97B240644C5a5a8A99c0F9Eb4aa74a754dE1"
 
-      window.TreasureContract.transfer(toAddress, 190000000, (error, txHash) => {
-          // it returns tx hash because sending tx
-          console.log(txHash);
-      });
+      let tokenAddress = "0xab20a3630f943831a57c767ee94b90801b88787c"
+
+      let wallet = '0x45ca230b2684D10bE2F8a749CA2F60828A2576f4' ;
+
+      // window.TreasureContract.transfer(toAddress, 190000000, (error, txHash) => {
+      //     // it returns tx hash because sending tx
+      //     console.log(txHash);
+      // });
+
+
+      let privateKey = Buffer.from('1C7A3993B45DFFE6403004AF68AEE24BBA8C6D5C5F31CDDA0B271D27364C4A67', 'hex')
+
+        // 1e18 === 1 HST
+        let amount = window.web31.utils.toHex(199e18)
+
+        window.web31.eth.getTransactionCount(wallet)
+          .then((count) => {
+            let rawTransaction = {
+              'from': wallet,
+              'gasPrice': window.web31.utils.toHex(20 * 1e9),
+              'gasLimit': window.web31.utils.toHex(210000),
+              'to': tokenAddress,
+              'value': 0x0,
+              'data': window.TreasureContractNew1.methods.transfer(toAddress, amount).encodeABI(),
+              'nonce': window.web31.utils.toHex(count)
+            }
+            let transaction = new ethereumjs.Tx(rawTransaction)
+            transaction.sign(privateKey)
+            window.web31.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
+              .on('transactionHash', console.log)
+          })
+
 
       // var data = contract.transfer.getData("0x2...", 10000, {from: "0x9..."});
   }
